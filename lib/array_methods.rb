38c65ea0ac39934c35d86ff7df0dea3fe634f811
Taishawn King <taishawnking1@yahoo.com>
2019-11-06 02:50:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,13 @@
 def using_include(array, element)
-
+using_include.include
 end
 
 def using_sort(array)
-
+using_sort.sort
 end
 
 def using_reverse(array)
-
+using_reverse.reverse
 end
 
 def using_first(array)
